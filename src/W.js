ad6355c4b77972a3dc23aaf6253fd7636d5e1551
pyrsmk <dev@dreamysource.fr>
2011-11-06 23:51:01 +0100

    Add onresize support

--- src/W.js
+++ src/W.js
@@ -16,29 +16,44 @@
     Get viewport width
     
     Parameters
-        boolean,number ems: if true return em-based width
-                            if a number translate it in ems
+        boolean, number, Function spec: if true, return em-based width
+                                        if a number, translate it in ems
+                                        if a function, will be called when the window has been resized
     
     Return
-        integer
+        integer, null
 */
-this.W=function(ems){
+this.W=function(spec){
+    // Catch resize event
+    if(typeof spec=='function'){
+        var addEventListener=window.addEventListener,
+            attachEvent=window.attachEvent;
+        if(addEventListener){
+            addEventListener("resize",spec,false);
+        }
+        else if(attachEvent){
+            attachEvent("onresize",spec);
+        }
+        // Force first call
+        spec();
+        return;
+    }
+    // Calculate em unit
     var doc=document.documentElement,
         unit=1;
-    // Calculate em unit
-    if(ems){
+    if(spec){
         var element=document.createElement('div');
         element.style.width='10em';
         doc.appendChild(element);
         unit=element.offsetWidth/10;
         doc.removeChild(element);
         // Tranlate provided px-based width
-        if(typeof ems=='number'){
-            return ems/unit;
+        if(typeof spec=='number'){
+            return spec/unit;
         }
     }
     // Guess window width
-    if(typeof ems!='number'){
+    if(typeof spec!='number'){
         var win=window,
             viewport_width=doc.offsetWidth,
             window_width=(typeof win.innerWidth)=='number'?win.innerWidth:doc.clientWidth;
