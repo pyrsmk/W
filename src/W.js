12cb52a40fa86ae8515a07cd0f96876a5385c4cf
pyrsmk <dev@dreamysource.fr>
2011-11-06 16:02:04 +0100

    Add width to em support

--- src/W.js
+++ src/W.js
@@ -6,49 +6,42 @@
     Homepage    : https://github.com/pyrsmk/W
     License     : MIT
     
-    Some reading
+    Some readings
     
         http://www.quirksmode.org/mobile/viewports.html
         http://www.quirksmode.org/mobile/tableViewport.html
 */
 
-(function(name,def){
-    if(typeof module!='undefined'){
-        module.exports=def;
-    }
-    else{
-        this[name]=def;
-    }
-}('W',function(){
+/*
+    Get viewport width
     
-    /*
-        Get viewport width
-        
-        Parameters
-            boolean ems: true to retrieve em-based width
-        
-        Return
-            integer
-    */
-    return function(ems){
-        /*
-            en tant que développeur web, il est important d'avoir la taille relative de la fenêtre du navigateur et pas du viewport car les scrollbars pour les navigateurs de bureau changent légèrement cette largeur et ceci est moins intuitif pour la prise en charge et l'adaptation d'un design; néanmoins, sur les mobiles c'est la taille du viewport qui va primer car la récupération de la taille de la fenetre n'est pas aisée (surtout avec Opera Mobile qui donne la taille de l'écran non relative)
-        */
-        // à inclure dans molt?
-        // Guess window width
-        var viewport_width=document.documentElement.offsetWidth,
-            window_width=(typeof window.innerWidth)=='number'?window.innerWidth:document.documentElement.clientWidth,
-            width=(window_width-viewport_width)*100/window_width<5?window_width:viewport_width;
-        // Translate it in ems
-        if(ems){
-            var element=document.createElement('div'),
-                html=document.documentElement;
-            element.style.width='10em';
-            html.appendChild(element);
-            width/=element.offsetWidth/10;
-            html.removeChild(element);
+    Parameters
+        boolean,number ems: if true return em-based width
+                            if a number translate it in ems
+    
+    Return
+        integer
+*/
+this.W=function(ems){
+    var doc=document.documentElement,
+        unit=1;
+    // Calculate em unit
+    if(ems){
+        var element=document.createElement('div');
+        element.style.width='10em';
+        doc.appendChild(element);
+        unit=element.offsetWidth/10;
+        doc.removeChild(element);
+        // Tranlate provided px-based width
+        if(typeof ems=='number'){
+            return ems/unit;
         }
-        return width;
-    };
-    
-}()));
+    }
+    // Guess window width
+    if(typeof ems!='number'){
+        var win=window,
+            viewport_width=doc.offsetWidth,
+            window_width=(typeof win.innerWidth)=='number'?win.innerWidth:doc.clientWidth;
+        return ((window_width-viewport_width)*100/window_width<5?window_width:viewport_width)/unit;
+    }
+};
