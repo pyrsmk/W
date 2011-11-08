f28a26e8cd273b27412b91caecd8bfb2f1b5f383
pyrsmk <dev@dreamysource.fr>
2011-11-08 21:43:54 +0100

    Fix heavy bug and save about 20 bytes

--- src/W.js
+++ src/W.js
@@ -1,7 +1,7 @@
 /*
     W, width management tool for responsive designs
 
-    Version     : 0.1.1
+    Version     : 0.1.2
     Authors     : Aurélien Delogu (dev@dreamysource.fr)
     Homepage    : https://github.com/pyrsmk/W
     License     : MIT
@@ -17,8 +17,18 @@
         To Lawrence Carvalho (carvalho@uk.yahoo-inc.com) for his useful TextResizeDetector script :)
 */
 
-this.W=function(){
-
+/*
+    Main function
+    
+    Parameters
+        boolean, number, Function spec: if true, return em-based window width
+                                        if a number, translate it to ems
+                                        if a function, will be called when the user resizes the window, zooms the contents or changes text size
+    
+    Return
+        integer, null
+*/
+this.W=function(spec){
     var win=window,
         doc=document,
         html=doc.documentElement,
@@ -29,72 +39,57 @@ this.W=function(){
         viewport_width,
         window_width,
         a,b;
-    
-    /*
-        Main function
-        
-        Parameters
-            boolean, number, Function spec: if true, return em-based window width
-                                            if a number, translate it to ems
-                                            if a function, will be called when the user resizes the window, zooms the contents or changes text size
-        
-        Return
-            integer, null
-    */
-    return function(spec){
-        if(typeof spec=='function'){
-            // Catch window resize event
-            if(a=win.addEventListener){
-                a('resize',spec,false);
-            }
-            else{
-                win.attachEvent('onresize',spec);
-            }
-            // Catch text resize event
-            if(!listeners.length){
-                textelement=doc.createElement('span');
-                textelement[style].position='absolute';
-                textelement[style].left='-999em';
-                textelement.innerHTML='&nbsp;';
-                html.appendChild(textelement);
-                textheight=textelement.offsetHeight;
-                setInterval(function(){
-                    // Trigger text resize event
-                    if(textheight!=(b=textelement.offsetHeight)){
-                        a=listeners.length;
-                        while(a){
-                            listeners[--a]();
-                        }
+    if(typeof spec=='function'){
+        // Catch window resize event
+        if(a=win.addEventListener){
+            a('resize',spec,false);
+        }
+        else{
+            win.attachEvent('onresize',spec);
+        }
+        // Catch text resize event
+        if(!listeners.length){
+            textelement=doc.createElement('span');
+            textelement[style].position='absolute';
+            textelement[style].left='-999em';
+            textelement.innerHTML='&nbsp;';
+            html.appendChild(textelement);
+            textheight=textelement.offsetHeight;
+            setInterval(function(){
+                // Trigger text resize event
+                if(textheight!=(b=textelement.offsetHeight)){
+                    a=listeners.length;
+                    while(a){
+                        listeners[--a]();
                     }
-                    textheight=b;
-                },250);
-            }
-            listeners.push(spec);
-            return;
+                }
+                textheight=b;
+            },250);
         }
-        var unit=1;
-        if(spec){
-            // Calculate em unit
-            a=doc.createElement('div');
-            a[style].width='1em';
-            html.appendChild(a);
-            unit=a.offsetWidth;
-            html.removeChild(a);
-            // Tranlate provided px-based width
-            if(typeof spec=='number'){
-                return spec/unit;
-            }
+        listeners.push(spec);
+        return;
+    }
+    var unit=1;
+    if(spec){
+        // Calculate em unit
+        a=doc.createElement('div');
+        a[style].width='1em';
+        html.appendChild(a);
+        unit=a.offsetWidth;
+        html.removeChild(a);
+        // Tranlate provided px-based width
+        if(typeof spec=='number'){
+            return spec/unit;
         }
-        if(typeof spec!='number'){
-            // Viewport width
-            a=html.offsetWidth;
-            // Window width
-            b=(typeof win.innerWidth)=='number'?
-              win.innerWidth:
-              html.clientWidth;
-            // Guess correct "window" width
-            return ((b-a)*100/b<5?b:a)/unit;
-        }
-    };
-    
-}();
+    }
+    if(typeof spec!='number'){
+        // Viewport width
+        a=html.offsetWidth;
+        // Window width
+        b=(typeof win.innerWidth)=='number'?
+          win.innerWidth:
+          html.clientWidth;
+        // Guess correct "window" width
+        return ((b-a)*100/b<5?b:a)/unit;
+    }
+};
