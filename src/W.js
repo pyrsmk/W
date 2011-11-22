4f55dde7e218d496c6b620edbad50cd89187b2a9
pyrsmk <dev@dreamysource.fr>
2011-11-22 17:19:28 +0100

    Fix multiple instance bug

--- src/W.js
+++ src/W.js
@@ -1,7 +1,7 @@
 /*
     W, width management tool for responsive designs
 
-    Version     : 0.1.4
+    Version     : 0.1.5
     Authors     : Aurélien Delogu (dev@dreamysource.fr)
     Homepage    : https://github.com/pyrsmk/W
     License     : MIT
@@ -17,18 +17,8 @@
         To Lawrence Carvalho (carvalho@uk.yahoo-inc.com) for his useful TextResizeDetector script :)
 */
 
-/*
-    Main function
+this.W=function(){
     
-    Parameters
-        boolean, number, Function spec: if true, return em-based window width
-                                        if a number, translate it to ems
-                                        if a function, will be called when the user resizes the window, zooms the contents or changes text size
-    
-    Return
-        integer, null
-*/
-this.W=function(spec){
     var win=window,
         doc=document,
         html=doc.documentElement,
@@ -39,61 +29,73 @@ this.W=function(spec){
         appendChild='appendChild',
         offsetHeight='offsetHeight',
         offsetWidth='offsetWidth',
-        listeners=[],
-        unit,
-        type=typeof spec,
-        a,
-        b;
-    if(type=='function'){
-        // Catch window resize event
-        if(a=win.addEventListener){
-            a('resize',spec,false);
+        listeners=[];
+    
+    /*
+        Main function
+        
+        Parameters
+            boolean, number, Function spec: if true, return em-based window width
+                                            if a number, translate it to ems
+                                            if a function, will be called when the user resizes the window, zooms the contents or changes text size
+        
+        Return
+            integer, null
+    */
+    return function(spec){
+        var type=typeof spec,unit,a,b;
+        if(type=='function'){
+            // Catch window resize event
+            if(a=win.addEventListener){
+                a('resize',spec,false);
+            }
+            else{
+                win.attachEvent('onresize',spec);
+            }
+            // Catch text resize event
+            if(!listeners.length){
+                textelement=doc[createElement]('span');
+                textelement[style].position='absolute';
+                textelement[style].top='-99em';
+                textelement.innerHTML='W';
+                html[appendChild](textelement);
+                textheight=textelement[offsetHeight];
+                setInterval(function(a,b){
+                    // Trigger text resize event
+                    if(textheight!=(b=textelement[offsetHeight])){
+                        a=listeners.length;
+                        while(a){
+                            listeners[--a]();
+                        }
+                    }
+                    textheight=b;
+                },250);
+            }
+            listeners.push(spec);
+            return;
+        }
+        // Compute em unit
+        a=doc[createElement]('div');
+        a[style].width='1em';
+        html[appendChild](a);
+        unit=a[offsetWidth];
+        unit=unit?unit:16; // because IE6/7 returns a zero offsetWidth
+        html.removeChild(a);
+        // Tranlate provided px-based width
+        if(type=='number'){
+            return spec/unit;
         }
         else{
-            win.attachEvent('onresize',spec);
+            // Viewport width
+            a=html[offsetWidth];
+            // Window width
+            if(!(b=win.innerWidth)){
+                b=html.clientWidth;
+            }
+            // Guess the correct "window" width
+            a=(b-a)*100/b<5?b:a;
+            return spec?a/unit:a;
         }
-        // Catch text resize event
-        if(!listeners.length){
-            textelement=doc[createElement]('span');
-            textelement[style].position='absolute';
-            textelement[style].top='-99em';
-            textelement.innerHTML='W';
-            html[appendChild](textelement);
-            textheight=textelement[offsetHeight];
-            setInterval(function(a,b){
-                // Trigger text resize event
-                if(textheight!=(b=textelement[offsetHeight])){
-                    a=listeners.length;
-                    while(a){
-                        listeners[--a]();
-                    }
-                }
-                textheight=b;
-            },250);
-        }
-        listeners.push(spec);
-        return;
-    }
-    // Compute em unit
-    a=doc[createElement]('div');
-    a[style].width='1em';
-    html[appendChild](a);
-    unit=a[offsetWidth];
-    unit=unit?unit:16; // because IE6/7 returns a zero offsetWidth
-    html.removeChild(a);
-    // Tranlate provided px-based width
-    if(type=='number'){
-        return spec/unit;
-    }
-    else{
-        // Viewport width
-        a=html[offsetWidth];
-        // Window width
-        if(!(b=win.innerWidth)){
-            b=html.clientWidth;
-        }
-        // Guess the correct "window" width
-        a=(b-a)*100/b<5?b:a;
-        return spec?a/unit:a;
-    }
-};
+    };
+    
+}();
