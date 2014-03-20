0890635bb053517ac52fd11c9d234f5791ee78a9
pyrsmk <dev@dreamysource.fr>
2014-03-20 18:04:58 +0100

    Update unit tests

--- unit/tests.js
+++ unit/tests.js
@@ -1,35 +1,34 @@
-domReady(function(){
+domready(function(){
 
-    sink('W',function(test,ok,before,after){
+	asyncTest('Events',function(){
+		expect(2);
+		alert("Please trigger responsive events (resize window, zoom-in, zoom-out, text resize");
+		var first,
+			second;
+		W(function(){
+			if(!first){
+				first=true;
+				ok(true,'Event catched by first listener');
+			}
+		});
+		W(function(){
+			if(!second){
+				second=true;
+				ok(true,'Event catched by second listener');
+				start();
+			}
+		});
+	});
 
-        test('Events',2,function(){
-            log('You\'ll must call events tests three times in order to catch window resize, zoom and text resize event');
-            var a,b;
-            W(function(){
-                if(!a){
-                    ok(true,'Event catched by first listener');
-                }
-                a=true;
-            });
-            W(function(){
-                if(!b){
-                    ok(true,'Event catched by second listener');
-                }
-                b=true;
-            });
-        });
+	test('Provided width',function(){
+		expect(1);
+		ok(W(1024)==64,'Format px width in ems [not optimized to pass with text size changing]');
+	});
 
-        test('Specific width',1,function(){
-            ok(W(1024)==64,'Format px width in ems [not optimized to pass with text size changing]');
-        });
-
-        test('Window width',1,function(){
-            alert("Is it the good width? : "+W());
-            ok(W(true)==W(W()),'Get current window width in ems');
-        });
-
-    });
-
-    start();
+	test('Window width',function(){
+		expect(1);
+		alert("Is "+W()+" it the right current width?");
+		ok(W(true)==W(W()),'Get current window width in ems');
+	});
 
 });
