ecbc4447c01100b51dc6cc2edf085ae017b120bb
pyrsmk <dev@dreamysource.fr>
2014-03-21 11:09:22 +0100

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,28 @@
 var gulp = require('gulp');
-// Disabled since gulp-jshint has too many directories which crash some things with Windows and Git
+/*
+	Disabled since gulp-jshint has too many directory depth which bring issues in Windows and Git
+*/
 //var jshint = require('gulp-jshint');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var clean = require('gulp-clean');
 var fs = require('fs');
 
-gulp.task('default',function(){
-	// Remove old files
+// Remove old files
+gulp.task('clean',function(){
 	gulp.src('*.min.js',{read:false})
 		.pipe(clean());
-	// Lint
+});
+
+// Lint
+gulp.task('lint',function(){
 	/*gulp.src('src/*.js')
 		.pipe(jshint())
 		.pipe(jshint.reporter('default'));*/
-	// Minify
+});
+
+// Minify
+gulp.task('minify',function(){
 	gulp.src('src/*.js')
 		.pipe(uglify())
 		.pipe(rename(function(path){
@@ -23,3 +31,5 @@ gulp.task('default',function(){
 		}))
 		.pipe(gulp.dest('.'));
 });
+
+gulp.task('default',['clean','lint','minify']);
