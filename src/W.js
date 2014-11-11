bb9f879652c0511243ed5f614ccf5be2d7abfcf1
pyrsmk <dev@dreamysource.fr>
2014-11-11 12:15:25 +0100

    Update AMD declaration

--- src/W.js
+++ src/W.js
@@ -14,13 +14,13 @@
 
 ;(function(context,name,definition){
 	if(typeof module!='undefined' && module.exports){
-		module.exports=definition();
+		module.exports=definition;
 	}
 	else if(typeof define=='function' && define.amd){
 		define(definition);
 	}
 	else{
-		context[name]=definition();
+		context[name]=definition;
 	}
 }(this,'W',function(){
 
@@ -175,4 +175,4 @@
 		}
 	};
 
-}));
+}()));
