07435a281632e4c4a0b1298477409c293f90b2ca
pyrsmk <dev@dreamysource.fr>
2015-10-06 09:16:39 +0200

    Build

--- src/W.js
+++ src/W.js
@@ -1,4 +1,4 @@
-/*! W 1.3.2 (https://github.com/pyrsmk/W) */
+/*! W 1.3.3 (https://github.com/pyrsmk/W) */
 
 ;(function(context, name, definition) {
 	if(typeof module!='undefined' && module.exports) {
